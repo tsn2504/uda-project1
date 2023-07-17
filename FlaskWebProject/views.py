b45a6ada197af3f21bc4830a5f1591e2cd7e4d39
Nguyen Tran <tsn2504@gmail.com>
2023-07-17 16:11:02 +0700

    remove unused

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -7,7 +7,7 @@ from flask import render_template, flash, redirect, request, session, url_for
 from werkzeug.urls import url_parse
 from config import Config
 from FlaskWebProject import app, db
-from FlaskWebProject.forms import LoginForm, PostForm, RemoveImage
+from FlaskWebProject.forms import LoginForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
 import msal
